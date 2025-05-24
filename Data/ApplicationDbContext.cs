27094c1deabec410399b92ca425bc483cd840d30
distronics001 <meim6condo@gmail.com>
2025-05-24 15:06:40 -0400

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SubscriptionApi.Models;
+
+namespace SubscriptionApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace SubscriptionApi.Models { 
+public class User
+{
+    public int Id { get; set; }
+    public required string Username { get; set; }
+    public required string PasswordHash { get; set; }
+    public required string Role { get; set; }
+    public bool IsSubscribed { get; set; }
+    public DateTime? SubscriptionEndDate { get; set; }
+}
+
+public class LoginModel
+{
+    public required string Username { get; set; }
+    public required string Password { get; set; }
+    } 
+}
